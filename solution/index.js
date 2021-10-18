41baa0e742d02352a9a2450c78b924ae14cbc708
Алена Кашина <zigafiddler@mail.ru>
2021-10-18 22:33:10 +0300

    Поправлено фунция вычисления

--- solution/index.js
+++ solution/index.js
@@ -2,27 +2,29 @@ module.exports = function (Homework) {
   const {add, less} = Homework;
   return async function (array, fn, initialValue, cb) {
     const asyncArrayGet = (idx) => new Promise(resolve => {
-      array.get(idx, number => resolve(number) )
+      asyncArray.get(idx, number => resolve(number) )
     })
     const asyncArrayLength = () => new Promise(resolve => {
-      array.length((len)=> resolve(len))
+      asyncArray.length((len)=> resolve(len))
     })
     const summ = (a ,b) => new Promise(resolve => {
       add(a, b, cb => resolve(cb))
     })
+
     const lessCheck = (a,b) => new Promise(resolve => {
       less(a, b, cb => resolve(cb))
     })
 
     const lengthArray = await asyncArrayLength()
-    let i = 0
-    let summArray = 0
-    while (await lessCheck(i,lengthArray)) {
-      let item = await asyncArrayGet(i)
-      summArray = await summ(summArray, item)
-      i = await summ(i, 1)
+    let index = 0
+    let result = initialValue ?? 0
+    while (await lessCheck(index,lengthArray)) {
+      let item = await asyncArrayGet(index)
+      result = await new Promise(resolve => {
+        fn(result, item, index, array, resolve)
+      });
+      index = await summ(index, 1)
     }
-    summArray = await summ(summArray, initialValue)
-    cb(summArray)
+    cb(result)
   }
 }
